73eeb8504e3c480a1f7b65911b8d999baf8f35b6
Salvo <jspkay@gmail.com>
2025-12-23 18:50:35 +0100

    Fixed brightness algorithm

--- dot_local/scripts/executable_brightness.py
+++ dot_local/scripts/executable_brightness.py
@@ -7,16 +7,17 @@ print(sys.argv)
 MAX = int(os.popen("brightnessctl m").read())
 current = os.popen("brightnessctl g").read()
 current = int(current)/MAX
+inc = 0.05
 print(current)
 if sys.argv[1] == "plus":
-    if current == 0.01:
-        os.system("brightnessctl s 5%")
-    elif current <= 0.96:
-        os.system("brightnessctl s +5%")
+    next = current + 1.9*inc # add `inc` percent
+    next = next - (next % inc) # floor to have `inc` increment
 elif sys.argv[1] == "minus":
-    if current >= 0.06:
-        os.system("brightnessctl s 5%-")
-    else:
-        os.system("brightnessctl s 1%")
+    next = current - 0.9*inc # remove `inc` percent
+    next = next - (next % inc) # floor it 
+    if next <= 0.01:
+        next = 0.01
 else:
     raise Exception("Error! Argument not compatible!")
+print(next)
+os.system(f"brightnessctl s {int(next*100)}%")
